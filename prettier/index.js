d1dfa491fa1d5bc4fb6e1315b673bfb723ce1a06
Shawn Koski <shawnk@aplos.com>
2023-01-14 10:54:51 -0800

    pettier

---
+++ prettier/index.js
@@ -0,0 +1,16 @@
+module.exports ={
+	"semi": false,
+	"singleQuote": true,
+	"trailingComma": "es5",
+	"tabWidth": 2,
+	"useTabs": true,
+	"printWidth": 80,
+	"overrides": [
+	{
+		"files": ["*.json", "*.yaml", "*.yml", "*.md", "*.markdown"],
+		"options": {
+			"tabWidth": 2
+		}
+	}
+]
+}
